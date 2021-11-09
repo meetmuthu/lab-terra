a2891103ca1058f867e7a26f3a7992f1ae69ed86
meetmuthu <meet.king.muthu@gmail.com>
2021-11-09 12:39:44 +0530

    3rd VM

--- main.tf
+++ main.tf
@@ -33,3 +33,18 @@ resource "google_compute_instance" "myvm1" {
         }
         }
 }
+resource "google_compute_instance" "myvm2" {
+        name = "mythirdvm"
+        machine_type = "f1-micro"
+        zone = "us-central1-c"
+        network_interface {
+                network = "default"
+                access_config {
+                }
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+        }
+        }
+}
